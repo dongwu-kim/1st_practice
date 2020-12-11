493e01a7606259c63f860bd2c574ad47ccaa94cc
dongwu-kim <alduk7797@naver.com>
2020-12-11 09:06:57 +0900

    test_main

---
+++ test/js/media_paused.js
@@ -0,0 +1,24 @@
+const widthMediaList = [
+  window.matchMedia("(max-width: 439.9px)"), // mobile
+  window.matchMedia("(min-width: 440px) and (max-width: 767.9px)"), // ipad
+  window.matchMedia("(min-width: 768px)"), // ipad pro + pc
+];
+/*const headerBar = document.querySelector(".index-header");
+
+function handelMediaQuery() {
+  const 
+  if (widthMediaList[0].matches) {
+    console.log("hi");
+  } else if (widthMediaList[1].matches) {
+    console.log("nice");
+  } else if (widthMediaList[2].matches) {
+    console.log("bye");
+  }
+}*/
+
+function init() {
+  /*console.log(widthMediaList);
+  handelMediaQuery();*/
+}
+
+init();
