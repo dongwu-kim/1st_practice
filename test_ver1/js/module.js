039b5832568295b17dee17d2b66fb19e31abbc10
dongwu-kim <alduk7797@naver.com>
2020-12-18 11:55:33 +0900

    renewal_index

--- test_ver1/js/module.js
+++
@@ -1,9 +0,0 @@
-import { handlechairAnimation } from "./media_width.js";
-import { checkFunction } from "./projector.js";
-
-function init() {
-  handlechairAnimation();
-  checkFunction(`.project`);
-}
-
-init();
