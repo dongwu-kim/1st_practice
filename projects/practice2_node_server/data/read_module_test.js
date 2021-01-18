74f3f2e74666d5ae098fbcc5a7fa08ca58a7505e
dongwu-kim <alduk7797@naver.com>
2021-01-18 15:39:46 +0900

    node study

--- projects/practice2_node_server/data/read_module_test.js
+++
@@ -1,5 +0,0 @@
-var fs = require(`fs`);
-
-fs.readFile(`text.txt`, `utf8`, (err, data) => {
-  console.log(data);
-});
--- projects/practice2_node_server/main.js
+++ projects/practice2_node_server/main.js
@@ -1,44 +1,84 @@
-var http = require("http");
-var fs = require("fs");
-var url = require("url");
+const http = require("http"),
+  fs = require("fs"),
+  url = require("url"),
+  app = http.createServer((request, response) => {
+    const _url = request.url,
+      queryData = url.parse(_url, true).query,
+      pathName = url.parse(_url, true).pathname;
 
-var app = http.createServer((request, response) => {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var title = queryData.id;
-  console.log(queryData.id);
+    console.log(url.parse(_url, true));
 
-  if (_url == "/") {
-    title = `Welcome`;
-  }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, `utf8`, (err, description) => {
-    var template = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8" />
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
+    if (pathName === `/`) {
+      fs.readdir(`./data`, (err, fileList) => {
+        let list = `<ol>`;
+
+        for (let i = 0; i < fileList.length; i++) {
+          let splitfileList = fileList[i].split(`_`);
+          splitfileList = splitfileList[1]; // 0 = id or sort num, 1 = file name
+          console.log(fileList[i]);
+          console.log(splitfileList);
+          list = list + `<li><a href = "?id=${fileList[i]}"/>${splitfileList}</a></li>`;
+        }
+
+        list = list + `</ol>`;
+
+        if (queryData.id !== undefined) {
+          /* let list = `<ol>
           <li><a href="?id=HTML">HTML</a></li>
           <li><a href="?id=CSS">CSS</a></li>
           <li><a href="?id=JS">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p style="margin-top: 45px">
-          ${description}
-        </p>
-      </body>
-    </html>
-    `;
-    response.end(template);
+          </ol>`; */
+
+          fs.readFile(`./data/${queryData.id}`, `utf8`, (err, description) => {
+            const title = queryData.id;
+            const template = `
+            <!DOCTYPE html>
+            <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8" />
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p style="margin-top: 45px">
+                  ${description}
+                </p>
+              </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+        } else {
+          const title = `Welcome`;
+          const description = `Hi, Node.js`;
+          const template = `
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8" />
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p style="margin-top: 45px">
+                ${description}
+              </p>
+            </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        }
+      });
+    } else {
+      response.writeHead(404);
+      response.end(`Not found`);
+    }
   });
-});
 
 app.listen(3002);
